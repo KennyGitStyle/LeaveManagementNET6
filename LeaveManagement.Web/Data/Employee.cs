2ff7e40e4de0dd92985c176239b54a527014484c
Kenneth Hendricks <kennethhendricks10@gmail.com>
2022-02-11 11:39:09 +0000

    Create Entity, Migrate and Update Database.

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public int NationalInsuranceNo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string LeaveName { get; set; } = null!;
+        public int DefaultDays { get; set; }
+       
+    }
+}
