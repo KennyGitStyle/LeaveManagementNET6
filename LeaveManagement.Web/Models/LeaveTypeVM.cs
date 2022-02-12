002c1a8cd2aff8dedae88897ae8636d57de3fb90
Kenneth Hendricks <kennethhendricks10@gmail.com>
2022-02-12 18:36:04 +0000

    LeaveType Create, Read, Update and Delete. Create LeaveType View with bootstrap styling.

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Leave Type Name")]
+        public string LeaveName { get; set; } = null!;
+        [Required]
+        [Display(Name = "Default Number Of Days")]
+        [Range(1, 25, ErrorMessage = "Please enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
